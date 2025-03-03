512b279fed4de7cc8f6d6a637e95cbeda13ce94a
Miguel Da Silva <miguel@dasilvaf.net>
2025-03-03 20:49:43 +0100

    add font. Add duplicate-line(). add use-package

--- init.el
+++ init.el
@@ -17,9 +17,26 @@
 (setq custom-file '"~/.emacs.d/custom.el")
 (load-file custom-file)
 
+(set-frame-font "FiraCode Nerd Font Mono 16" nil t)
+
 (straight-use-package 'use-package)
 
 (use-package gruber-darker-theme
   :straight t
   :config
   (load-theme 'gruber-darker t))
+
+(defun duplicate-line ()
+  "Duplicate current line"
+  (interactive)
+  (let ((column (- (point) (point-at-bol)))
+	(line (let ((s (thing-at-point 'line t)))
+		(if s (string-remove-suffix "\n" s) ""))))
+    (move-end-of-line 1)
+    (newline)
+    (insert line)
+    (move-beginning-of-line 1)
+    (foward-char column)))
+
+(global-set-key (kbd "C-,") 'duplicate-line)
+    
